65987f489a55e2489252023cf83c6843dcb990ae
Atri Saxena <atrisaxena2@gmail.com>
2022-12-31 20:08:42 +0530

    Feature creation

---
+++ data_to_features.py
@@ -0,0 +1,54 @@
+from tqdm import tqdm
+import os 
+from utils.util import *
+import argparse
+import numpy as np
+from tempfile import TemporaryFile
+
+# def arg_parse():
+#     parser = argparse.ArgumentParser(description="Wake-Word create features")
+
+class Data2Features:
+    def __init__(self, target_class:str, data_path):
+        self.target_class = target_class
+        self.data_path = data_path
+        self.target_label = 0
+        self.non_target_label = 1
+        self.features = [] 
+        self.classes = os.listdir(self.data_path).remove("_background_noise_")
+        self.non_target_class = ['seven'] #self.classes.remove(self.target_class)
+        print(f"Following Classes are present:{os.listdir(self.data_path)}")
+    
+    def create_features(self):
+        self.create_non_target_class()
+        self.create_target_class()
+        # Balance dataset later 
+        self.features = np.asarray(self.features, dtype=object )
+        return create_train_test_split(self.features[:,0], self.features[:,1])
+
+    def create_non_target_class(self):
+        for non_t_class in self.non_target_class:
+            print(f"Creating feature for {non_t_class} Non Targeted Class.")
+            for file_name in tqdm(os.listdir(os.path.join(self.data_path, non_t_class))):
+                lfbe_feature = create_lfbe_feature(file_name=os.path.join(self.data_path, non_t_class, file_name))
+                self.features.append((lfbe_feature, self.non_target_label))
+    
+    def create_target_class(self):
+        print(f"Creating features for target class: {self.target_class}")
+        for file_name in tqdm(os.listdir(os.path.join(self.data_path, self.target_class))):
+            lfbe_feature = create_lfbe_feature(file_name=os.path.join(self.data_path, self.target_class, file_name))
+            self.features.append((lfbe_feature, self.target_label))
+
+
+DATASET_PATH = "B:\Datasets\speech_commands_v0.01"
+TARGET_CLASS_NAME = "stop"
+NON_TARGET_CLASS_NAME = ["seven"]
+
+data2features = Data2Features(target_class=TARGET_CLASS_NAME, 
+                                data_path=DATASET_PATH)
+X_train, X_test, y_train, y_test = data2features.create_features()
+print(X_train.shape, y_train.shape)
+np.savez("wakeword_features", X_train = X_train, 
+                            X_test = X_test,
+                            y_train = y_train,
+                            y_test = y_test)
---
+++ utils/util.py
@@ -0,0 +1,42 @@
+
+import librosa
+import os 
+import matplotlib.pyplot as plt 
+import python_speech_features
+from sklearn.model_selection import train_test_split
+
+DATASET_PATH = "B:\Datasets\speech_commands_v0.01"
+CLASS_NAME = "stop"
+
+def create_lfbe_feature(file_name):
+
+    samples, sample_rate = librosa.load(file_name)
+    lfbe_feature = python_speech_features.base.logfbank(samples, 
+                                                        samplerate=sample_rate,
+                                                        winlen=0.025,
+                                                        winstep=0.01,
+                                                        nfilt=64,
+                                                        nfft=551)
+    return lfbe_feature[:76][:]
+
+def create_train_test_split(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42, shuffle=True)
+    return X_train, X_test, y_train, y_test
+
+def Wave2Spectogram():
+    file_name = os.path.join(DATASET_PATH, CLASS_NAME , "0bd689d7_nohash_2.wav")
+
+    samples, sample_rate = librosa.load(file_name)
+    print(sample_rate)
+    logfbank = python_speech_features.base.logfbank(samples, samplerate=sample_rate, nfilt=64, nfft=551)
+    logfbank = logfbank[:76][:]
+    print(logfbank.shape)
+    plt.imshow(logfbank, cmap="inferno", origin="lower")
+    plt.show()
+    # fig, ax = plt.subplots(figsize = (5,5))
+
+    # ax.set_axis_off()
+    # ax.specgram(samples, Fs=2)
+    # fig.savefig("spec.png", bbox_inches="tight", pad_inches=0)
+    # plt.close(fig)
+    #del sample_rate, ax, fig, samples
