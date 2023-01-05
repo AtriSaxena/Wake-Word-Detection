74ba251e51b7ce022b0edbc2d94e6f082a36bcc3
Atri Saxena <atrisaxena2@gmail.com>
2023-01-05 16:19:51 +0530

    Update model file

--- models/model.py
+++ models/model.py
@@ -9,9 +9,9 @@ class WakeWordModel(nn.Module):
     """
     def __init__(self):
         super(WakeWordModel, self).__init__()
-        self.conv_1 = nn.Sequential(nn.Conv2d(in_channels = 64, 
+        self.conv_1 = nn.Sequential(nn.Conv2d(in_channels = 1, 
                                 out_channels = 96,
-                                kernel_size= (9,5),
+                                kernel_size = (9,5)
                                 ),
                        nn.MaxPool2d(kernel_size=(2,3),
                        ))
@@ -22,25 +22,31 @@ class WakeWordModel(nn.Module):
                                         stride=(3,1)),
                                     nn.MaxPool2d(kernel_size=(1,2)))
         self.conv_3 = nn.Conv2d(in_channels=192, out_channels=192,
-                                    kernel_size=(4,3))
-        self.conv_4 = nn.Conv2d(192,192,(3,3))
+                                    kernel_size=(4,3),
+                                    stride=1)
+        self.conv_4 = nn.Conv2d(192,192,3)
         self.conv_5 = nn.Conv2d(192,192,kernel_size=3)
         self.flatten = nn.Flatten()
-        self.dense_1 = nn.Linear(in_features=1728, out_features=500)
+        self.dense_1 = nn.Linear(in_features=960, out_features=500)
         self.dense_2 = nn.Linear(in_features=500, out_features=500)
         self.output = nn.Linear(in_features=500, out_features=1) 
-        self.softmax = nn.Softmax(1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.relu(self.conv_1(x))
+
         x = self.relu(self.conv_2(x)) 
+
         x = self.relu(self.conv_3(x))
+
         x = self.relu(self.conv_4(x))
+
         x = self.relu(self.conv_5(x))
-        x = self.flatten() 
+
+        x = self.flatten(x) 
         x = self.relu(self.dense_1(x)) 
         x = self.relu(self.dense_2(x)) 
-        x = self.softmax(self.output(x))
+        x = self.sigmoid(self.output(x))
         return x
 
 model = WakeWordModel()
