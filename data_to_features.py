3b37570f5d27befa7014e4b0ddea7cc23a7ddbbb
Atri Saxena <atrisaxena2@gmail.com>
2023-01-05 22:59:58 +0530

    Add sampling balance

--- data_to_features.py
+++ data_to_features.py
@@ -1,22 +1,18 @@
 from tqdm import tqdm
 import os 
 from utils.util import *
-import argparse
 import numpy as np
 from tempfile import TemporaryFile
-
-# def arg_parse():
-#     parser = argparse.ArgumentParser(description="Wake-Word create features")
 
 class Data2Features:
     def __init__(self, target_class:str, data_path):
         self.target_class = target_class
         self.data_path = data_path
-        self.target_label = 0
-        self.non_target_label = 1
+        self.target_label = 1
+        self.non_target_label = 0
         self.features = [] 
         self.classes = os.listdir(self.data_path).remove("_background_noise_")
-        self.non_target_class = ['seven'] #self.classes.remove(self.target_class)
+        self.non_target_class = self.classes.remove(self.target_class) #['seven'] 
         print(f"Following Classes are present:{os.listdir(self.data_path)}")
     
     def create_features(self):
@@ -24,25 +20,28 @@ class Data2Features:
         self.create_target_class()
         # Balance dataset later 
         self.features = np.asarray(self.features, dtype=object )
-        return create_train_test_split(self.features[:,0], self.features[:,1])
+        X, y = make_balanced_class(features=self.features)
+        return create_train_test_split(X,y)
 
     def create_non_target_class(self):
         for non_t_class in self.non_target_class:
             print(f"Creating feature for {non_t_class} Non Targeted Class.")
             for file_name in tqdm(os.listdir(os.path.join(self.data_path, non_t_class))):
                 lfbe_feature = create_lfbe_feature(file_name=os.path.join(self.data_path, non_t_class, file_name))
-                self.features.append((lfbe_feature, self.non_target_label))
+                if lfbe_feature.shape[0] == 76:
+                    self.features.append((lfbe_feature, self.non_target_label))
     
     def create_target_class(self):
         print(f"Creating features for target class: {self.target_class}")
         for file_name in tqdm(os.listdir(os.path.join(self.data_path, self.target_class))):
             lfbe_feature = create_lfbe_feature(file_name=os.path.join(self.data_path, self.target_class, file_name))
-            self.features.append((lfbe_feature, self.target_label))
+            if lfbe_feature.shape[0] == 76:
+                self.features.append((lfbe_feature, self.target_label))
 
 
 DATASET_PATH = "B:\Datasets\speech_commands_v0.01"
 TARGET_CLASS_NAME = "stop"
-NON_TARGET_CLASS_NAME = ["seven"]
+#NON_TARGET_CLASS_NAME = ["seven"]
 
 data2features = Data2Features(target_class=TARGET_CLASS_NAME, 
                                 data_path=DATASET_PATH)
--- utils/util.py
+++ utils/util.py
@@ -1,10 +1,13 @@
-
 import librosa
 import os 
 import matplotlib.pyplot as plt 
 import python_speech_features
 from sklearn.model_selection import train_test_split
 import numpy as np 
+from imblearn.over_sampling import SMOTE 
+from imblearn.under_sampling import RandomUnderSampler 
+from imblearn.pipeline import Pipeline 
+from collections import Counter
 
 DATASET_PATH = "B:\Datasets\speech_commands_v0.01"
 CLASS_NAME = "stop"
@@ -18,7 +21,8 @@ def create_lfbe_feature(file_name):
                                                         winstep=0.01,
                                                         nfilt=64,
                                                         nfft=551)
-    return lfbe_feature[:76][:]
+    print(f"lfbe features:{lfbe_feature.shape}")
+    return lfbe_feature[:76][:][:]
 
 def create_train_test_split(X,y):
     X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42, shuffle=True)
@@ -35,17 +39,26 @@ def load_features(file_name = "wakeword_features.npz"):
     y_test = features['y_test']
     return X_train, y_train, X_test, y_test
 
+def make_balanced_class(features):
+    print(f"Class Distribution before Sampling: {Counter(features[:,1])}")
+    over = SMOTE(sampling_strategy=0.1) 
+    under = RandomUnderSampler(sampling_strategy=0.5) 
+    steps = [('o', over),('u', under)]
+    pipeline = Pipeline(steps=steps) 
+    X, y = pipeline.fit_resample(features[:,0], features[:,1]) 
+    print(f"Class Distribution after Sampling: {Counter(y)}")
+    return X, y
 
 def Wave2Spectogram():
     file_name = os.path.join(DATASET_PATH, CLASS_NAME , "0bd689d7_nohash_2.wav")
-
     samples, sample_rate = librosa.load(file_name)
     print(sample_rate)
     logfbank = python_speech_features.base.logfbank(samples, samplerate=sample_rate, nfilt=64, nfft=551)
-    logfbank = logfbank[:76][:]
+    logfbank = logfbank[:76][:][:]
     print(logfbank.shape)
-    plt.imshow(logfbank, cmap="inferno", origin="lower")
-    plt.show()
+    plt.imshow(logfbank)
+    #plt.show()
+    plt.savefig("logfbank.png", bbox_inches="tight", pad_inches=0 )
     # fig, ax = plt.subplots(figsize = (5,5))
 
     # ax.set_axis_off()
@@ -53,3 +66,4 @@ def Wave2Spectogram():
     # fig.savefig("spec.png", bbox_inches="tight", pad_inches=0)
     # plt.close(fig)
     #del sample_rate, ax, fig, samples
+Wave2Spectogram()
